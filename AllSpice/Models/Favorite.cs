52d6546f1eb3b628272e7200c9092b23ad5ed5e9
William Edwards <williamedwards7717@gmail.com>
2023-05-01 16:22:57 -0600

    ingredients done

---
+++ AllSpice/Models/Favorite.cs
@@ -0,0 +1,9 @@
+namespace AllSpice.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+        public string AccountId { get; set; }
+    }
+}
